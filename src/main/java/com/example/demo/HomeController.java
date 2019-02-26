ce332ff5d9f76ec4ba7c3940b90432a4bfa3f6f9
jihyunle <jennifer.jlee@outlook.com>
2019-02-26 11:48:15 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/carform") // get request create an empty object
+    public String loadCarForm(Model model){
+        model.addAttribute("car", new Car());
+        return "carform";
+
+    }
+
+
+    @PostMapping("/carform") // post request validates the input
+    public String processCarForm(@Valid Car car, BindingResult result){
+        if (result.hasErrors()){
+            return "carform";
+        }
+        return "carconfirm";
+
+    }
+}
